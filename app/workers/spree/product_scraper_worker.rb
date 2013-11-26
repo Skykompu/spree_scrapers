5e7b97abcc79d13ce4d483e29ad4df255269da7c
GarPit <garik.piton@gmail.com>
2013-11-26 18:24:21 +0400

    using special sidekiq queue name

--- app/workers/spree/product_scraper_worker.rb
+++ app/workers/spree/product_scraper_worker.rb
@@ -2,6 +2,7 @@ require 'sidekiq'
 module Spree
   class ProductScraperWorker
     include ::Sidekiq::Worker
+    sidekiq_options :queue => :scraper, :backtrace => true
 
     def perform(taxon_id, scraper_id, taxon_url)
       taxon = Spree::Taxon.find(taxon_id)
--- app/workers/spree/scraper_worker.rb
+++ app/workers/spree/scraper_worker.rb
@@ -2,6 +2,7 @@ require 'sidekiq'
 module Spree
   class ScraperWorker
     include ::Sidekiq::Worker
+    sidekiq_options :queue => :scraper, :backtrace => true
 
     def perform(scraper_id)
       scraper = Spree::Scraper.find(scraper_id)
