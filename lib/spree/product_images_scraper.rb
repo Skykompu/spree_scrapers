7e3184796c4388ae556ed9a3f835e9762050025b
Skykompu <kompu_f@hotmail.com>
2016-07-21 06:20:35 +0400

    Update product_images_scraper.rb
    add URI.encode(..) - fix links with white-spaces or %20
    add image_links.length > 0 -optimization
    add log.info for information of process

--- lib/spree/product_images_scraper.rb
+++ lib/spree/product_images_scraper.rb
@@ -12,25 +12,35 @@ module Spree
     end
 
     def scrape
-      log.info "Parsing images for product #{product.name}"
       if can_scrape_images?
         scrape_images
+      else
+        if product.images.exists?
+          log.info "product.images.exists #{product.id}"
+        elsif !product_url.present?
+          log.info "product url no present: #{product_url.present?}"
+        end 
       end
     end
 
     def scrape_images
       page = Nokogiri::HTML(open(URI.join(scraper.catalog_url, product_url)))
       image_links = page.css(scraper.product_images_selector)
-      image_links.each_with_index do |image_link, image_index|
-        scrape_image(image_link[:href], image_index)
+      if image_links.length > 0
+        image_links.each_with_index do |image_link, image_index|
+          scrape_image(image_link[:href], image_index)
+        end
+      else
+        log.info "not images for product #{product_url}"
       end
     end
 
     def scrape_image(image_url, image_index)
-      image_file = open(URI.join(scraper.catalog_url, image_url))
+      image_file = open(URI.join(scraper.catalog_url, URI.encode(image_url)))
       def image_file.original_filename
         base_uri.path.split('/').last
       end
+      log.info "Parsing images for product #{product.name}"
       image = product.images.create(:attachment => image_file, :alt => product.name + '-' + (image_index + 1).to_s)
       image_file.close
     end
