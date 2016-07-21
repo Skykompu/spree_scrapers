102c6036f5a5edc7fe32b4366d0bbcc143533412
Skykompu <kompu_f@hotmail.com>
2016-07-21 06:12:16 +0400

    Update products_scraper.rb
    delete funcionality scrape_properties
    delete funcionality scrape_variants

--- lib/spree/products_scraper.rb
+++ lib/spree/products_scraper.rb
@@ -48,43 +48,17 @@ module Spree
     end
 
     def scrape_product(product_url, product_name)
-      log.info "Parsing product #{product_name}"
-      page = Nokogiri::HTML(open(URI.join(scraper.catalog_url, product_url)))
-      product_description = nil
-      if scraper.product_description_selector.present?
-        product_description_element = page.css(scraper.product_description_selector)
-        product_description = product_description_element.text
+      if product = Spree::Product.where(name: product_name).first
+        log.info "Parsing product exist #{product_name}"
+        scrape_images(product, product_url) 
+      else
+        log.info "product not exist #{product_name}"
       end
-      # don't know why but "first_or_create" not working
-      unless (product = Spree::Product.where(name: product_name).first)
-        product = Spree::Product.create!(name: product_name, description: product_description, price: 1, shipping_category_id: Spree::ShippingCategory.first.id, sku: [taxon.id, SecureRandom.hex(2)].join)
-        taxon.products << product
-      end
-      scrape_properties(product, page)
-      scrape_images(product, product_url)
-      scrape_variants(product, product_url)
-    end
-
-    def scrape_properties(product, product_page)
-      property_value_elements = product_page.css(scraper.property_value_selector).map(&:text).map(&:strip)
-      product_page.css(scraper.property_name_selector).map(&:text).map(&:strip).each_with_index do |property_name, index|
-        if valid_property?(property_name)
-          property = Spree::Property.where(name: property_name).first_or_create!(presentation: property_name)
-          product.product_properties.where(property_id: property.id).first_or_create!(value: property_value_elements[index]) if property_value_elements[index]
-        end
-      end
-    end
-
-    def valid_property?(property_name)
-      property_name.present? # this could be overridden
     end
 
     def scrape_images(product, product_url)
         ProductImagesScraperWorker.perform_async(product.id, scraper.id, product_url)
     end
 
-    def scrape_variants(product, product_url)
-        ProductVariantsScraperWorker.perform_async(product.id, scraper.id, product_url)
-    end
   end
 end
